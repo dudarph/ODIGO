1e016e8a7b41a994b16f63ab744a300360d74f99
dudarph <136990020+dudarph@users.noreply.github.com>
2023-11-10 20:41:56 +0400

    main.js

--- js/main.js
+++ js/main.js
@@ -26,10 +26,12 @@
 		menu.classList.remove("header__nav_active");
 	});
 	if (window.innerWidth < 768) {
-		for (let i = 0; i < menuLinks.length; i += 1) {
-			menuLinks[i].addEventListener("click", () => {
-				menu.classList.remove("header__nav_active");
-			});
+		if (window.innerWidth < 768) {
+			for (let i = 0; i < menuLinks.length; i += 1) {
+				menuLinks[i].addEventListener("click", () => {
+					menu.classList.remove("header__nav_active");
+				});
+			}
 		}
 	}
 })();
